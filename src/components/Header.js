036cef68ae434d95e313c8d7acc0ba7aac7cf56c
Arjun I <arjun.iyyappan.2005@gmail.com>
2024-09-30 22:38:21 +0530

    built Home-Page

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import "../App.css";
+
+export default function Header(){
+    return (
+        <header>
+            <div></div>
+            <button>Home</button>
+            <button>About</button>
+            <button>Tracks</button>
+            <button>Speakers</button>
+            <button>Sponsors</button>
+            <button>FAQs</button>
+            <button>Apply</button>
+        </header>
+    );
+};
