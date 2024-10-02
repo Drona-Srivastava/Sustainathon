9bcc0a52bcc1131b4c48e53585dd31685a869dea
sai krishna <saikrishnasprasad2642005@gmail.com>
2024-10-02 23:41:43 +0530

    Speakers page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './About.js'
+import Speakers from './Speakers.js';
 
 function App() {
   return (
     <>
-    <About/>
+    <Speakers/>
     </>
   );
 }
---
+++ src/Speakers.js
@@ -0,0 +1,37 @@
+import './Speakers.css'
+import icon from './icon.jpeg'
+function Speakers()
+{
+    return(
+        <>
+   <div className="chat-container">
+   <div className="profile-picture">
+        <img src={icon} alt="Profile" />
+      </div>
+      <div className="arrow"></div>
+      <div className="chat-bubble">
+        {/* Add text or content in the chat bubble here */}
+      </div>
+    </div>
+    <div className="chat-container2">
+      <div className="profile-picture2">
+        <img src={icon} alt="Profile" />
+      </div>
+      <div className="arrow2"></div>
+      <div className="chat-bubble">
+        {/* Add text or content in the chat bubble here */}
+      </div>
+    </div>
+    <div className="chat-container">
+      <div className="profile-picture">
+        <img src={icon} alt="Profile" />
+      </div>
+      <div className="arrow"></div>
+      <div className="chat-bubble">
+        {/* Add text or content in the chat bubble here */}
+      </div>
+    </div>
+        </>
+    );
+}
+export default Speakers;
