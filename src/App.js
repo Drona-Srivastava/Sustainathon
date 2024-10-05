44aa58017eb44860587cd00af483a1c98b295eb0
Arjun I <arjun.iyyappan.2005@gmail.com>
2024-10-05 08:10:55 +0530

    added sponsor page and made nav-bar fixed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import NavBar from './components/Header';
 import HomeSym from './components/homesym';
 import Countdown from './components/countdown';
+import Sponsors from './sponsors';
 
 function App() {
   const target = new Date(2024,12,11);
   return (
-    <body className="body">
+    <body>
       <NavBar></NavBar>
-      <HomeSym></HomeSym>
-      <Countdown targetDate={target}></Countdown>
+      <div className="body">
+        <HomeSym></HomeSym>
+        <Countdown targetDate={target}></Countdown>
+      </div>
+      <Sponsors></Sponsors>
     </body>
   );
 }
---
+++ src/sponsors.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+export default function Sponsors(){
+    return (
+        <>
+        <div className="sponsor-body">
+            <h1 className="wording">Special thanks to our sponsors<img src="https://s3-alpha-sig.figma.com/img/76fb/7f70/3c2bf7f27baf807260f6dd85b96aa396?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RddxgXdvhhOdaQOy-GlEG9iJeWuayPPsKcIo0ZPlpYxb2xnCCpUizmAKlHSZcdVf2Hpd8aqWyRy1fEml12NeQlSPaJ4bK5yJaXIHSiSEdKdbL8op9fbNnvncUNsVeSidNazjhW19bi9uXMR6jTKFSLfnI2YJEcSV3iF7rkYuXP1GbiidwJ80RdJuZWVNDo7pkWByGChhwcoHvIKW4ae~UbCBRUN6czMYVMmOhli3y3sFqBfxqhUF~DWrXVVy7ooIMbvyngpXiGYGBEG6IiYU1f8fNGpcJ-CIBr7p73Eef~7KDxAEse~q3-8EtxQChzDBg61e~fZ~xEzD3PsHxC7N3Q__" alt="Hand_img"></img></h1>
+            <div className="sponsors"></div>
+            <div className="sponsors"></div>
+            <div className="sponsors"></div>
+            <div className="sponsors"></div>
+            <div className="info">
+                <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum cursus, metus at cursus bibendum, est erat ullamcorper metus, vitae fermentum nulla sem eu arcu. Integer lacinia elit ac ipsum scelerisque, at fermentum elit vulputate. Fusce euismod metus ut justo vehicula, nec hendrerit sapien elementum. Sed maximus, nunc vel tincidunt cursus, nisi justo viverra magna, eu convallis nisi lectus nec libero. Donec bibendum est sit amet felis laoreet, nec consectetur velit iaculis. Integer ut leo auctor, varius ligula eu, tristique libero. Curabitur auctor facilisis lorem, eu vehicula libero elementum id. Vivamus cursus est in cursus efficitur.</p>
+            </div>
+        </div>
+        </>
+    );
+}
