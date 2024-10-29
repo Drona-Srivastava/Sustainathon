2aa796be8de1b5f83018a77c501bcdab1de70ee1
Arjun I <arjun.iyyappan.2005@gmail.com>
2024-10-29 15:51:30 +0530

    Added animations and completed page

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -3,8 +3,11 @@ import '../sponsors.css';
 export default function Sponsors(){
     return (
         <>
-        <div className="sponsors">
-            <p className="wording">Special Thanks to our sponsors <img src="images/hand_img.png" className="hand_img" alt="Hand_img"></img></p>
+        <div className="sponsor">
+            <div className="lean">
+                <span></span>
+            </div>
+            <p className="word">Special Thanks to our sponsors <img src="images/hand_img.png" className="hand_img" alt="Hand_img"></img></p>
             <div className="sponsors-name sp1"></div>
             <div className="sponsors-name sp2"></div>
             <div className="sponsors-name sp3"></div>
