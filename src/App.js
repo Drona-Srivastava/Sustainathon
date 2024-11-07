56285a8d7d35165907a5a83718c90dca9580172a
sai krishna <saikrishnasprasad2642005@gmail.com>
2024-11-07 21:14:23 +0530

    Tracks Page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import About from './About.js'
 import Speakers from './Speakers.js';
+import Tracks from './tracks.js';
 
 function App() {
   return (
     <>
-    <Speakers/>
+    <Tracks></Tracks>
     </>
   );
 }
---
+++ src/tracks.js
@@ -0,0 +1,42 @@
+import './Tracks.css'
+import track1 from './track1p.png'
+import track2 from './track2-removebg-preview.png'
+import track3 from './track3-removebg-preview.png'
+function Tracks()
+{
+    return(
+        <>
+       
+        <div className="circle-container">
+            
+                <div className="border-1"></div>
+    <div className="border-2"></div>
+    <div className="border-3"></div>
+    <div className="blur-circle blur-circle-1"></div>
+    <div className="blur-circle blur-circle-2"></div>
+    <div className="blur-circle blur-circle-3"></div>
+    
+    <div className = "white-circle2"></div>
+    <div className = "white-circle3"></div>
+
+        <div className="center-circle">
+          <div className="tracks">TRACKS</div>
+         </div>
+         
+      </div>
+
+      <div className="circle1">
+        <img src = {track2}></img>
+       
+        </div>
+        <div className="circle2">
+       <img src = {track1}></img>
+        </div>
+        <div className="circle3">
+       <img src = {track3}></img>
+        </div>
+      </>
+    
+    );
+}
+export default Tracks
